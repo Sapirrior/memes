d9e1c290191fbad662e5d9dfa922d4d441b5b5ce
sapirrior <jimmynnit>
2025-05-10 20:01:06 +0530

    update

---
+++ gork/gork.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(intents=intents)
+@bot.event async def on_ready(): print('online')
+bot.run(TOKEN)
